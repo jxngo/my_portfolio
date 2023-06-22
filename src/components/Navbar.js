24caa9f6cbf301bb6afe7fed084a6c4948c61966
Johnny Ngo <johnnyhngo@yahoo.com>
2023-06-22 01:04:32 -0400

    new update+

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import styled from "@emotion/styled";
+import speaker from '../assets/speaker.png';
 
 function Navbar() {
   const NavContainer = styled.div `
+  img {
+    margin-top: 3rem;
+    width: 50rem;
+  }
   `
   
   const NavBar = styled.div `
-    
+
     ul {
       list-style-type: none;
       display: flex;    
@@ -26,16 +31,14 @@ function Navbar() {
       border-radius: 50%;
       padding: 3rem;
     }
-
   `
     const LabelDiv = styled.div`
+      margin-top: 4rem;
       ul {
-        
-        margin-right: 2.8rem;
+        margin-right: 3rem;
       }
       li {
-        width: 50%;
-        padding: 0rem;
+        width: 40%;
       }
       text{
         font-size: 1.5rem;
@@ -45,55 +48,69 @@ function Navbar() {
     `
     const Button1 = styled.button`
       background: #Ed8008;
-      
-      box-shadow: 3px -3px 1px 1px #1c1c1c;
+      box-shadow: 3px -3px 5px 1px #1c1c1c;
       a {
-        
         background: #Ed8008;
       }
     `
     const Button2 = styled.button`
       background: #ED3F1C;
-      box-shadow: 3px -1px 1px 2px #1c1c1c;
+      box-shadow: 3px -1px 5px 2px #1c1c1c;
       a {
         background: #ED3F1C;
-        
       }
     `
     const Button3 = styled.button`
       background: #BF1B1B;
-      box-shadow: 0px -4px 1px 2px #1c1c1c;
+      box-shadow: 0px -4px 5px 2px #1c1c1c;
       a {
         background: #BF1B1B;
       }
     `
     const Button4 = styled.button`
       background: #736B1E;
-      box-shadow: -3px -3px 1px 2px #1c1c1c;
+      box-shadow: -3px -3px 5px 2px #1c1c1c;
       a {
         background: #736B1E;
       }
     `
     const Button5 = styled.button`
-      box-shadow: -3px -3px 1px 1px #1c1c1c;
+      box-shadow: -3px -3px 5px 1px #1c1c1c;
       background: #736356;
       a {
         background: #736356;
       }
     `
-
-
+    const ButtonDiv = styled.div `
+    
+    p {
+      font-size: 0.6rem;
+      font-family: Akzidenz Grotesk, Arial, sans-serif;
+    }
+    
+    button {
+      box-shadow: 0px -2px 5px 2px #1c1c1c;
+      margin-top: 1.5rem;
+      width: 13rem;
+      height: 13rem;
+      border-radius: 10%;
+      background-color: #AAB7BF;
+      border: none;
+      
+    }
+    span {
+      height: 11rem;
+      width: 11rem;
+      background: linear-gradient(#99a4ab 65%, #d4dbdf);
+      border-radius: 50%;
+      display: inline-block;
+    }
+  `
   return (
     <NavContainer>
+      <img src={speaker} alt="speaker"></img>
       <NavBar>
-        <ul>
-          <li><Button1><a href="#about"> </a></Button1></li>
-          <li><Button2><a href="#skills"> </a></Button2></li>
-          <li><Button3><a href="#projects"> </a></Button3></li>
-          <li><Button4><a href="#experiences"> </a></Button4></li>
-          <li><Button5><a href="#contact"> </a></Button5></li>
-        </ul>
-        <LabelDiv>
+      <LabelDiv>
           <ul>
             <li><text>About</text></li>
             <li><text>Skills</text></li>
@@ -102,7 +119,22 @@ function Navbar() {
             <li><text>Contact</text></li>
           </ul>
         </LabelDiv>
+        <ul>
+          <li><Button1><a href="#about"> </a></Button1></li>
+          <li><Button2><a href="#skills"> </a></Button2></li>
+          <li><Button3><a href="#projects"> </a></Button3></li>
+          <li><Button4><a href="#experiences"> </a></Button4></li>
+          <li><Button5><a href="#contact"> </a></Button5></li>
+        </ul>
       </NavBar>
+      <ButtonDiv>
+          <p>Download CV</p>
+          <a href="/resume/JNResume.pdf" download>
+            <button>
+              <span></span>
+            </button>
+          </a>
+        </ButtonDiv>
     </NavContainer>
   )
 }
--- src/sections/Introduction.js
+++ src/sections/Introduction.js
@@ -1,11 +1,8 @@
 import React from 'react'
 import styled from "@emotion/styled";
-import speaker from '../assets/speaker.png';
-
 
 function Introduction() {
   const IntroContainer = styled.div`
-    
     padding-top: 5rem;
     h1 {
         color: #372E2D;
@@ -20,65 +17,16 @@ function Introduction() {
         font-family: Helvetica, Arial, sans-serif;
         padding-left: 5rem;
       }
-    p {
-      margin-top: 2rem;
-      font-size: 0.7rem;
-      font-family: Akzidenz Grotesk, Arial, sans-serif;
-    }
-    
-    img {
-      border-radius: 50%;
-      animation: rotation 10s infinite linear;
-      @keyframes rotation {
-        from {
-          transform: rotate(0deg);
-        }
-        to {
-          transform: rotate(359deg);
-        }
-      }
-    }
-    button {
-      box-shadow: 0px -2px 1px 2px #1c1c1c;
-      margin-top: 3rem;
-      width: 15rem;
-      height: 15rem;
-      border-radius: 10%;
-      background-color: #AAB7BF;
-      border: none;
-      
-    }
-    span {
-      height: 13rem;
-      width: 13rem;
-      background: linear-gradient(#99a4ab 65%, #d4dbdf);
-      border-radius: 50%;
-      display: inline-block;
-    }
   `
   const IntroDiv = styled.div `
     text-align: left;
   `
-  const ButtonDiv = styled.div `
-    text-align: center;
-  `
 
   return (
      <IntroContainer>
-       <img src={speaker} alt="speaker"></img>
        <IntroDiv>
-        <h1>Hi! I'm Johnny Ngo</h1>
-        <h3>Software Engineer | Audio & Visual</h3>
-        <ButtonDiv>
-          <a href="/resume/JNResume.pdf" download>
-            <button>
-              <span></span>
-            </button>
-          </a>
-        </ButtonDiv>
        </IntroDiv>
-       <p>Download CV</p>
-     
+       
      </IntroContainer>
     
   )
