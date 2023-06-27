48a63b1cc2590a2d89e42b9aea947dfe83610b56
Johnny Ngo <johnnyhngo@yahoo.com>
2023-06-27 00:42:47 -0400

    color change

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import './App.css';
-import Experiences from './sections/Experiences';
-import Contact from './sections/Contact';
-import Projects from './sections/Projects';
 import Navbar from './components/Navbar';
-import Introduction from './sections/Introduction';
 import About from './sections/About';
 import Skills from './sections/Skills';
+import Experiences from './sections/Experiences';
+import Projects from './sections/Projects';
+import Contact from './sections/Contact';
+
 
 function App() {
   return (
     <div className="App">
     <header><Navbar></Navbar></header>
     <main>
-      <section id="intro"><Introduction></Introduction></section>
       <section id="about"><About></About></section>
       <section id="skills"><Skills></Skills></section>
       <section id="projects"><Projects></Projects></section>
       <section id="experiences"><Experiences></Experiences></section>
       <section id="contact"><Contact></Contact></section>
     </main>
-      
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,52 +19,116 @@ function Navbar() {
       
     }
   `
-  const NavBar = styled.div `
-    padding-bottom: 2rem;
-    ul {
-      list-style-type: none;
-      display: flex; 
-      justify-content: space-between; 
-    }
-    li {
-      margin-right: 2rem;
-      padding-left: 1rem;
-    }
-    button {
-      border: none;
-      border-radius: 50%;
-      padding: 1rem;
-    }
-    a{
-      color: transparent;
-      display: inline-block;
-      border-radius: 50%;
-      padding: 3rem;
-    }
-  `
-  const LabelDiv = styled.div`
-    margin-top: 4rem; 
-   
-    ul {
+  const FlexContainer = styled.div `
       display: flex;
-      justify-content: space-around;
-      list-style: none;
-      padding-bottom: 2rem;
-    }
-    li {
-      margin-right: 2rem;
-      padding-left: 2rem;
-    }
-    text{
-      font-size: 2.2rem;
-      font-family: Satoshi;
-      @media (min-width: 425px) {
-        font-size: 2.5rem;
+      
+  `
+  const ButtonDiv = styled.div`
+      margin-top: 2rem;
+      margin-left: 3rem;
+      flex: 1;
+      text {
+        font-size: 1.8rem;
+        font-family: Satoshi;
+      }
+      button {
+        box-shadow: 0px -2px 5px 2px #1c1c1c;
+        margin-top: 1.5rem;
+        width: 13rem; 
+        height: 13rem;
+        border-radius: 10%;
+        background-color: #AAB7BF;
+        border: none;
+      }
+      span {
+        height: 11rem;
+        width: 11rem;
+        background: rgb(170,183,191);
+        background: linear-gradient(180deg, rgba(170,183,191,1) 0%, rgba(153,164,171,1) 68%, rgba(212,219,223,1) 100%);
+        border-radius: 50%;
+        display: inline-block;
+        border: 0.01rem solid #bbc5cb;
+      }
+    `
+  const SocialDiv = styled.div`
+      flex: 1;
+      margin-top: 3rem;
+      div {
+        margin-top: 2rem;
+        margin-left: 4rem;
+        width: 32.8rem;
+        height: 8.1rem;
+        border: 0.1rem solid #1c1c1c;
+        border-radius: 1rem;
+        position: relative;
+        box-shadow: 0px -2px 2px 1px #1c1c1c;
+        background-color: #454545;
+      }
+      button {
+        margin: 0rem 0.1rem;
+        width: 8rem;
+        height: 8rem;
+        border-radius: 10%;
+        background-color: #5a7b95;
+        border: 1px solid #888;
+      }
+      span {
+        height: 6rem;
+        width: 6rem;
+        background: rgb(81,110,134);
+        background: linear-gradient(180deg, rgba(81,110,134,1) 0%, rgba(139,162,180,1) 68%, rgba(193,205,215,1) 100%);
+        border-radius: 50%;
+        display: inline-block;
+        border: 0.01rem solid #98adbd;
         
       }
-    }
-  `
-  const Button1 = styled.button`
+      a {
+        background-color: #5a7b95;
+        
+      }
+      img {
+        
+        width: 4rem;
+        border-radius: 50%;
+      }
+      ul {
+        display: flex;
+        justify-content: space-between;
+        margin-right: 2rem;
+      }
+      li {
+        display: inline;
+        padding-left: 4rem;
+      }
+    `
+
+    const ButtonsDiv = styled.div `
+      margin-top: 5rem;
+      display: flex;
+      justify-content: space-evenly;
+      width: 55rem;
+      margin-left: 2rem;
+      text{
+        font-size: 1.8rem;
+        font-family: Satoshi;
+        @media (min-width: 425px) {
+          font-size: 2.5rem; 
+        }
+      }
+      button {
+        margin-top: 2rem;  
+        border: none;
+        border-radius: 50%;
+        padding: 1rem;
+      }
+      a{
+        color: transparent;
+        display: inline-block;
+        border-radius: 50%;
+        padding: 3rem;
+      }
+    `
+    const Button1 = styled.button`
     background: #Ed8008;
     box-shadow: 3px -3px 5px 1px #1c1c1c;
     a {
@@ -99,116 +163,45 @@ function Navbar() {
         background: #736356;
       }
     `
-  const FlexContainer = styled.div `
-      display: flex;
-  `
-  const ButtonDiv = styled.div`
-      margin-top: 2rem;
-      margin-left: 3rem;
 
-      flex: 1;
-      text {
-        font-size: 2.2rem;
-        font-family: Satoshi;
-      }
-      button {
-        box-shadow: 0px -1px 5px 2px #1c1c1c;
-        margin-top: 1.5rem;
-        width: 13rem; 
-        height: 13rem;
-        border-radius: 10%;
-        background-color: #AAB7BF;
-        border: none;
-      }
-      span {
-        height: 11rem;
-        width: 11rem;
-        background: linear-gradient(#99a4ab 65%, #d4dbdf);
-        border-radius: 50%;
-        display: inline-block;
-      }
-    `
-  const SocialDiv = styled.div`
-      flex: 1;
-      margin-top: 3rem;
-      
-      div {
-        margin-top: 2.5rem;
-        margin-left: 4rem;
-        width: 32.8rem;
-        height: 8.1rem;
-        border: 0.1rem solid #1c1c1c;
-        border-radius: 1rem;
-        position: relative;
-        box-shadow: 0px -2px 2px 1px #1c1c1c;
-        background-color: #454545;
-      }
-      button {
-        margin: 0rem 0.1rem;
-        width: 8rem;
-        height: 8rem;
-        border-radius: 10%;
-        background-color: #315B7B;
-        border: 1px solid #888;
-      }
-      span {
-        height: 6rem;
-        width: 6rem;
-        background: linear-gradient(#2c516e 65%, #5a7b95);
-        border-radius: 50%;
-        display: inline-block;
-      }
-      a {
-        background-color: #315B7B;
-      }
-      img {
-        width: 4rem;
-        border-radius: 50%;
-      }
-      ul {
-        display: flex;
-        justify-content: space-between;
-        margin-right: 2rem;
-      }
-      li {
-        display: inline;
-        padding-left: 4rem;
-      }
-    `
   return (
     <NavContainer>
       <SpeakerDiv>
         <img src={speaker} alt="speaker"></img>
       </SpeakerDiv>
-      <LabelDiv>
-          <ul>
-            <li><text>About</text></li>
-            <li><text>Skills</text></li>
-            <li><text>Projects</text></li>
-            <li><text>Work</text></li>
-            <li><text>Contact</text></li>
-          </ul>
-      </LabelDiv>
-      <NavBar>
+      <ButtonsDiv>
         <div>
-          <ul>
-            <li><Button1><a href="#about"> </a></Button1></li>
-            <li><Button2><a href="#skills"> </a></Button2></li>
-            <li><Button3><a href="#projects"> </a></Button3></li>
-            <li><Button4><a href="#experiences"> </a></Button4></li>
-            <li><Button5><a href="#contact"> </a></Button5></li>
-          </ul>
+          <text>ABOUT</text>
+          <Button1><a href="#about"> </a></Button1>
         </div>
-      </NavBar>
+        <div>
+          <text>SKILLS</text>
+          <Button2><a href="#skills"> </a></Button2>
+        </div>
+        <div>
+          <text>PROJECTS</text>
+          <Button3><a href="#projects"> </a></Button3>
+        </div>
+        <div>
+          <text>WORK</text>
+          <Button4><a href="#experiences"> </a></Button4>
+        </div>
+        <div>
+        <text>CONTACT</text>
+        <Button5><a href="#contact"> </a></Button5>
+        </div>
+      </ButtonsDiv>
+      
       <FlexContainer>
         <SocialDiv>
           <ul>
-              <li> <img src={Github_logo} alt='github'></img></li>
-              <li> <img src={Linkedin_logo} alt='linkedin'></img></li>
-              <li> <img src={Instagram_logo} alt='instagram'></img></li>
-              <li> <img src={Spotify_logo} alt='spotify'></img></li>
+            <li> <img src={Github_logo} alt='github'></img></li>
+            <li> <img src={Linkedin_logo} alt='linkedin'></img></li>
+            <li> <img src={Instagram_logo} alt='instagram'></img></li>
+            <li> <img src={Spotify_logo} alt='spotify'></img></li>
           </ul>
           <div>
+
             <button>
               <a href="https://github.com/jxngo" target="_blank" rel="noopener noreferrer"><span></span></a>
             </button>
@@ -222,10 +215,11 @@ function Navbar() {
               <a href="https://open.spotify.com/user/jzngo?si=b23d3566f7184ced" target="_blank" rel="noopener noreferrer"><span></span></a>
             </button>
           </div>
+
         </SocialDiv>
         <ButtonDiv>
           <div>
-            <text>Download CV</text>
+            <text>DL CV</text>
             <a href="/resume/JNResume.pdf" download>
               <button>
                 <span></span>
