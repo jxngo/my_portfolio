ca4714caabea4e43e5afbf09a201bbf5b7e7b1e2
Johnny Ngo <johnnyhngo@yahoo.com>
2023-06-28 20:50:00 -0400

    a

--- src/sections/Projects.js
+++ src/sections/Projects.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from '@emotion/styled';
-import Slider from '../components/Slider';
+//import Slider from '../components/Slider';
 
 function Projects() {
   const ProjectsContainer = styled.div`
